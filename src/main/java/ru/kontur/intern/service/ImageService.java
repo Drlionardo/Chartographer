875cabe57139b95bcff43aa6ed14a885e8a46e32
Danila Iagupets <d.iagupets@gmail.com>
2022-03-05 23:12:09 +0300

    add 'path' comand line argument

--- src/main/java/ru/kontur/intern/service/ImageService.java
+++ src/main/java/ru/kontur/intern/service/ImageService.java
@@ -1,15 +1,19 @@
 package ru.kontur.intern.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.UUID;
 
 @Service
 public class ImageService {
+    @Value("#{springApplicationArguments.nonOptionArgs.get(0)}")
+    private String path;
 
     public String createCharta(int width, int height) {
-        return "ID";
+        return UUID.randomUUID().toString();
     }
 
     public void fillCharta(int width, int height, int x, int y, MultipartFile image) {
