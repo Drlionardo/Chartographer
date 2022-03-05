907913ef66bc908ee8a1542fe70be2378747c910
Danila Iagupets <d.iagupets@gmail.com>
2022-03-05 23:43:31 +0300

    implement image creation method

--- src/main/java/ru/kontur/intern/service/ImageService.java
+++ src/main/java/ru/kontur/intern/service/ImageService.java
@@ -4,7 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.UUID;
 
 @Service
@@ -13,7 +19,23 @@ public class ImageService {
     private String path;
 
     public String createCharta(int width, int height) {
-        return UUID.randomUUID().toString();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        String id = UUID.randomUUID().toString();
+        if(!Files.exists(Path.of(path))) {
+            try {
+                Files.createDirectory(Path.of(path));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        try {
+            ImageIO.write(image, "bmp", new File(path + "/" + id+ ".bmp"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return id;
     }
 
     public void fillCharta(int width, int height, int x, int y, MultipartFile image) {
