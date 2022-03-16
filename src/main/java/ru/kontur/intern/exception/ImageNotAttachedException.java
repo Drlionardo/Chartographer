38c96d7d82d9fd2932071767bb2813a3f4235783
Danila Iagupets <d.iagupets@gmail.com>
2022-03-16 23:42:57 +0300

    remove unused code

--- src/main/java/ru/kontur/intern/exception/ImageNotAttachedException.java
+++
@@ -1,7 +0,0 @@
-package ru.kontur.intern.exception;
-
-public class ImageNotAttachedException extends RuntimeException {
-    public ImageNotAttachedException(String message) {
-        super(message);
-    }
-}
